6290be5005e4123f847a641030538b7198320d08
soyedpork27 <121537173+soyedpork27@users.noreply.github.com>
2023-12-16 16:32:19 +0900

    [level unrated] Title: 몫 구하기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120805. 몫 구하기/몫 구하기.js
+++ 프로그래머스/unrated/120805. 몫 구하기/몫 구하기.js
@@ -1,3 +1,5 @@
-const solution = (num1, num2) => {
-    return Math.floor(num1/num2);
-}
+// const solution = (num1, num2) => {
+//     return Math.floor(num1/num2);
+// }
+
+const solution = (num1,num2) => ~~(num1/num2);
