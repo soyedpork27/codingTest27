749c77bf9d04dee9c948ac7c3edc910957f74bf6
soyedpork27 <121537173+soyedpork27@users.noreply.github.com>
2024-01-07 18:39:14 +0900

    [level 2] Title: 구명보트, Time: 10.46 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -1,25 +1,31 @@
 function solution(people, limit) {
+    
+    // 사람의 체중을 오름차순으로(작은 값 부터) 정렬
     people.sort((a,b) => (a-b));
     
+    // return 할 구명보트의 수를 최대로 잡는다.
     let count = people.length;
-    // 틀렸던 코드
-    // let count = 0;
     
+    // 낮은 체중부터 배열을 순회할 인덱스
     let i = 0;
+    // 높은 체중부터 배열을 순회할 인덱스
     let j = people.length-1;
     
+    // 반복문 실행
     while(i < j){
+        // 만약 두 사람의 체중이 제한보다 작은 경우 => 두 사람이 탑승 가능한 경우
         if(people[i]+people[j] <= limit){
+            // 구명보트의 수를 하나 줄인다.
             count--;
-            // 틀렸던 코드
-            // count++;
+            // 인덱스 조정
             i++;
             j--;
         }else{
-            // 틀렸던 코드
-            // count++;
+            // 혼자 탑승하는 경우
+            // 인덱스 조정
             j--;
         }
     }
+    // 구명보트의 수 리턴
     return count;
 }
