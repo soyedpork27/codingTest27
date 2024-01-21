dd04b1a7d5e7ec2c5fc02be2dbc252cf5c65a484
soyedpork27 <121537173+soyedpork27@users.noreply.github.com>
2024-01-21 13:47:07 +0900

    [level 2] Title: 주식가격, Time: 5.22 ms, Memory: 42.2 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,20 +1,20 @@
 const solution = (prices) => {
-    
     const n = prices.length;
     
     // 0으로 채워진 배열 생성
     const result = new Array(n).fill(0);
     
-    // n^2번 이중 반복문을 실행
+    // 이중 반복문을 실행
     for(let i = 0 ; i < n ; i++){
         for(let j = i+1; j < n; j++){
             // i 번째 값과 i+1번째 값을 비교 했을 때 가격이 떨어진다면
             if(prices[i] > prices[j]) {
-                // 결과배열의 i번째 값을 j-1로 할당하고 반복문을 탈출한다.
+                // 결과배열의 i번째 값을 j-i로 할당하고 반복문을 탈출한다.
                 result[i] = j-i;
                 break;
             }else {
                 // i번째 값에 1 씩 추가한다.
+                // 끝까지 떨어지지 않는다면 1초씩 증가!
                 result[i]++
             }
         }
