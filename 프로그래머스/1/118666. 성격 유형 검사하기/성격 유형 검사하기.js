c2e15aa876315f31e564cdf1c4998124d9c2cc5d
soyedpork27 <121537173+soyedpork27@users.noreply.github.com>
2024-04-07 11:21:10 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.38 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,5 +1,8 @@
 function solution(survey, choices) {
     
+    // 각 항목 당 하나씩만 잡고 점수 계산 시작
+    // 0 또는 양수인 경우 변수명 그대로 반환
+    // 음수인 경우 R -> T, C -> F, J -> M, A -> N
     let R = 0;
     
     let C = 0;
@@ -8,18 +11,20 @@ function solution(survey, choices) {
     
     let A = 0;
     
-    
-    
+    // 설문 하나당 반복
     survey.forEach((i,idx) => {
+        
         const target = i[0];
         
+        // 선택 점수 산출은 -4의 값을 해둔 뒤
         choices[idx] = -(choices[idx]-4);
         
         if(target==="R"||target==="T"){
-            
             if(target==="R"){
+                // 앞의 값이 변수와 일치하는 경우 변수에 값을 그대로 더해준다.
                 R = R+choices[idx];
             }else{
+                // 앞의 값이 변수의 반대인 경우 변수에서 값을 빼준다.
                 R = R-choices[idx]
             }
             
@@ -61,9 +66,7 @@ function solution(survey, choices) {
         
     });
     
-    
-    console.log(R,C,J,A)
-    
+    // 각각의 값이 0 이상인지 판별 후 그렇다면 변수명대로 그렇지 않다면 반대로 출력한다.
     return `${R>=0?"R":"T"}${C>=0?"C":"F"}${J>=0?"J":"M"}${A>=0?"A":"N"}`
     
 }
