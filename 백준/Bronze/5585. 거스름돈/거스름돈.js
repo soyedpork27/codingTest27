99fd8999b05d58cdde647b7a42082a2176a42779
keyBoardWarrior <121537173+soyedpork27@users.noreply.github.com>
2024-05-05 14:12:40 +0900

    [Bronze II] Title: 거스름돈, Time: 148 ms, Memory: 9484 KB -BaekjoonHub

--- 백준/Bronze/5585. 거스름돈/거스름돈.js
+++ 백준/Bronze/5585. 거스름돈/거스름돈.js
@@ -14,19 +14,21 @@ rl.on('line', (line) => {
 rl.on('close', () => {
   // 입력값이 input 배열에 저장됩니다.
     let a = input[0];
-    let number = (1000-parseInt(a)).toString().split('').map(i => parseInt(i));
+    // let number = (1000-parseInt(a)).toString().split('').map(i => parseInt(i));
     // 500엔, 100엔, 50엔, 10엔, 5엔, 1엔
-    // 이후에 필요한 작업을 수행합니다.
-    let ctn = 0;
- 
     
-    number.forEach((i) => {
-        if(i>=5){
-            ctn = ctn+i-4;
-        }else{
-            ctn = ctn+i;
-        }
-    });
+    const arr = [500,100,50,10,5,1];
+    
+    let num = 1000-parseInt(a);
+    let ctn = 0;
+    
+    for(let i of arr){
+        ctn = ctn + Math.floor(num/i);
+        num = num - (i * Math.floor(num/i));
+    }
     
     console.log(ctn);
+    
+    
+    
 });
