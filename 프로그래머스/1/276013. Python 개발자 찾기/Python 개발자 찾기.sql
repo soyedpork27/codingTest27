e47f597062e914acf75f316be58ab501a64e4abe
keyBoardWarrior <121537173+soyedpork27@users.noreply.github.com>
2024-10-02 00:56:24 +0900

    [level 1] Title: Python 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
+++ 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
@@ -1,5 +1,12 @@
 -- 코드를 작성해주세요
+# select ID, EMAIL, FIRST_NAME, LAST_NAME
+# from DEVELOPER_INFOS
+# where skill_1 = 'Python' or skill_2 = 'Python' or skill_3 = 'Python'
+# order by ID asc
+
+# ---
+
 select ID, EMAIL, FIRST_NAME, LAST_NAME
 from DEVELOPER_INFOS
-where skill_1 = 'Python' or skill_2 = 'Python' or skill_3 = 'Python'
+WHERE "Python" IN(SKILL_1, SKILL_2, SKILL_3)
 order by ID asc
