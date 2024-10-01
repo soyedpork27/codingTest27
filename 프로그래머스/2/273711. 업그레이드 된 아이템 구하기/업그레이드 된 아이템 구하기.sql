d674395c76885837a8566ad6e8578c2a4a49d86a
keyBoardWarrior <121537173+soyedpork27@users.noreply.github.com>
2024-10-02 01:31:05 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -1,11 +1,23 @@
 -- 코드를 작성해주세요
-select a.ITEM_ID, a.ITEM_NAME, a.RARITY
-from ITEM_INFO as a
-join item_tree as b
-on a.item_id = b.item_id
-where b.PARENT_ITEM_ID in (select c.item_id
-from ITEM_INFO as c
-join ITEM_TREE as d
-on c.ITEM_ID = d.item_id
-where c.rarity = 'RARE')
-order by a.ITEM_ID desc
+
+# 서브쿼리
+# select a.ITEM_ID, a.ITEM_NAME, a.RARITY
+# from ITEM_INFO as a
+# join item_tree as b
+# on a.item_id = b.item_id
+# where b.PARENT_ITEM_ID in (select c.item_id
+# from ITEM_INFO as c
+# join ITEM_TREE as d
+# on c.ITEM_ID = d.item_id
+# where c.rarity = 'RARE')
+# order by a.ITEM_ID desc
+
+# JOIN 활용
+SELECT a.ITEM_ID, a.ITEM_NAME, a.RARITY
+FROM ITEM_INFO AS a
+JOIN ITEM_TREE AS b
+ON a.ITEM_ID = b.ITEM_ID
+JOIN ITEM_INFO AS c
+ON b.PARENT_ITEM_ID = c.ITEM_ID
+WHERE c.RARITY = 'RARE'
+ORDER BY a.ITEM_ID desc;
