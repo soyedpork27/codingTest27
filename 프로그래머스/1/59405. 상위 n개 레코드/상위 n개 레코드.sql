56c89070769e9ddf42857f8a84d15fcef90e534b
keyBoardWarrior <121537173+soyedpork27@users.noreply.github.com>
2024-10-01 22:05:17 +0900

    [level 1] Title: 상위 n개 레코드, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59405. 상위 n개 레코드/상위 n개 레코드.sql
+++ 프로그래머스/1/59405. 상위 n개 레코드/상위 n개 레코드.sql
@@ -1,5 +1,7 @@
 -- 코드를 입력하세요
-SELECT NAME
-from ANIMAL_INS
-order by datetime asc
-limit 1
+# SELECT NAME
+# from ANIMAL_INS
+# order by datetime asc
+# limit 1;
+
+select NAME from ANIMAL_INS where DATETIME = (SELECT MIN(DATETIME) from ANIMAL_INS);
