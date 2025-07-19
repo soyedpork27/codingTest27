2c6de1a6c7df4daa90046401390146b675840be2
keyBoardWarrior <121537173+soyedpork27@users.noreply.github.com>
2025-07-19 20:40:57 +0900

    [level 2] Title: 올바른 괄호, Time: 29.04 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,25 +1,27 @@
 function solution(s){
     
-    let ctn = 0;
-    
-    const arr = [];
+    const result = [];
     
     s.split('').forEach((str) => {
         
-        if(arr[0]===')'){
+        // 만약 결과 배열의 첫 번째 값이 닫힌 괄호라면 종료
+        if(result[0]===')'){
             return;
         }
         
-        if(str===')'&&arr[arr.length-1]==='('){
-            arr.pop();
+        // 만약 값이 닫힌 괄호이고 결과 배열의 마지막 값이 열린 괄호라면 결과 배열의 마지막 값(열린 괄호)을 삭제하고 종료
+        if(str===')'&&result[result.length-1]==='('){
+            result.pop();
             return;
         }
         
-        arr.push(str);
+        // 두 조건에 해당하지 않는 경우 값을 배열에 전송
+        result.push(str);
         
     });
     
-    return !arr.length?true:false;
+    // 배열의 길이가 0인 경우 올바른 괄호
+    return !result.length?true:false;
 
     
 }
